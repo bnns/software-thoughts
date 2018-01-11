755ea072323388730ac380c751ae72bba9896d2a
bnns <omethyn@gmail.com>
2018-01-10 21:56:23 -0600

    portfolio

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,21 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html
+    exposing
+        ( Html
+        , a
+        , ul
+        , li
+        , text
+        , div
+        , h1
+        , h3
+        , img
+        , section
+        , p
+        , span
+        )
+import Html.Attributes exposing (class, href, src, target)
 
 
 ---- MODEL ----
@@ -35,9 +49,53 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+    div [ class "container" ]
+        [ section [ class "header" ]
+            [ h1 [] [ text "Software" ]
+            , h1 [] [ text "Thoughts" ]
+            ]
+        , section [ class "about" ]
+            [ div []
+                [ p []
+                    [ text "Hi, I'm "
+                    , a [ target "_blank", href "https://github.com/bnns" ] [ text "Dennis Yao" ]
+                    , text ", a web developer"
+                    ]
+                , p [] [ text " based in Texas," ]
+                , p [] [ text "and this is my portfolio." ]
+                ]
+            ]
+        , section [ class "links" ]
+            [ div [] [ h3 [] [ text "projects" ] ]
+            , ul []
+                [ li []
+                    [ a [ target "_blank", href "https://menubase.xyz" ] [ text "menubase" ]
+                    , p [] [ text "a website generator for restaurants" ]
+                    ]
+                ]
+            , div [] [ h3 [] [ text "open source" ] ]
+            , ul []
+                [ li []
+                    [ a [ target "_blank", href "https://24.softwarethoughts.com" ] [ text "24" ]
+                    , a [ target "_blank", href "https://github.com/bnns/24" ] [ span [ class "fab fa-github" ] [] ]
+                    , p [] [ text "arithmetic card game based on Maths24" ]
+                    ]
+                , li []
+                    [ a [] [ text "front-end clones" ]
+                    , a [ target "_blank", href "https://github.com/bnns/clones" ] [ span [ class "fab fa-github" ] [] ]
+                    , p [] [ text "clones of popular apps in various frameworks" ]
+                    ]
+                , li []
+                    [ a [] [ text "ex_phaxio" ]
+                    , a [ target "_blank", href "https://github.com/bnns/ex_phaxio" ] [ span [ class "fab fa-github" ] [] ]
+                    , p [] [ text "elixir client for ", a [ href "https://www.phaxio.com/" ] [ text "phaxio" ] ]
+                    ]
+                , li []
+                    [ a [ target "_blank", href "https://softwarethoughts.com" ] [ text "this site" ]
+                    , a [ target "_blank", href "https://github.com/bnns/software-thoughts" ] [ span [ class "fab fa-github" ] [] ]
+                    ]
+                ]
+            ]
         ]
 
 
